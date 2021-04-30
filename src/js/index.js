e3ac3aa150e19b85656951737995e6d1f80b0243
play.js <git@gituser.com>
2021-04-30 18:32:39 +0900

    basic environment

---
+++ src/js/index.js
@@ -0,0 +1,6 @@
+window.onload = () => {
+  const $wrapperUl = document.getElementById('wrapper');
+  console.log($wrapperUl.nodeName);
+}
+
+console.log(`hello`);
---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    app: './src/js/index.js'
+  },
+  module: {
+    rules: [
+      {
+        test: (m) => { return /\.(js|jsx)$/.test(m) },
+        exclude: (m) => { return /node_modules/.test(m) },
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      },
+      {
+        test: (m) => { return /\.css$/.test(m) },
+        exclude: (m) => { return /node_modules/.test(m) },
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      },
+      {
+        test: (m) => { return /\.(png|jp(e*)g|svg)$/.test(m) },
+        exclude: (m) => { return /node_modules/.test(m) },
+        use: [{
+          loader: 'url-loader',
+          options: { 
+            limit: 8000,
+            name: 'images/[hash]-[name].[ext]'
+          } 
+        }]
+      }
+    ]
+  },
+  plugins: [],
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, './dist'),
+    publicPath: '/'
+  }
+};
